457df23227c9efd43ba66bf166ca1aa5dded7166
Piotr Mroczkowski <pietrek141@o2.pl>
2021-07-08 14:10:09 +0200

    tests incorrect

--- src/test/java/com/example/crudrestdemo/controller/ArtistControllerTest.java
+++ src/test/java/com/example/crudrestdemo/controller/ArtistControllerTest.java
@@ -39,7 +39,7 @@ class ArtistControllerTest {
         artist.setAlbums(Collections.singletonList(album));
         artistController.addArtist(artist);
         List<Artist> allArtist = artistController.getAllArtist().getBody();*/
-        int a = 10;
+        int a = 9;
 
         assertEquals(10, a);
     }
